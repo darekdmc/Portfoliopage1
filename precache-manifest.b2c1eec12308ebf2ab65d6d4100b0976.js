3bddddcd1b73b203f5a7e49a83cbc253b897a7cd
Darek <57916749+darekdmc@users.noreply.github.com>
2022-04-04 21:58:15 +0200

    Add files via upload

---
+++ precache-manifest.b2c1eec12308ebf2ab65d6d4100b0976.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a2764f0dfa7fb61453103797199cad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7edbd6dd8bdaa146e6bc",
+    "url": "/static/css/2.b328fd89.chunk.css"
+  },
+  {
+    "revision": "284eeccd6588dbcab3d0",
+    "url": "/static/css/main.5a1e5089.chunk.css"
+  },
+  {
+    "revision": "7edbd6dd8bdaa146e6bc",
+    "url": "/static/js/2.2a6a0ae6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.2a6a0ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "284eeccd6588dbcab3d0",
+    "url": "/static/js/main.b2277550.chunk.js"
+  },
+  {
+    "revision": "c981f58058c409d3e733",
+    "url": "/static/js/runtime-main.d4e7e7fc.js"
+  },
+  {
+    "revision": "5387ae661161ef9cdeaee343179bc44a",
+    "url": "/static/media/2.5387ae66.jpg"
+  },
+  {
+    "revision": "c6596c0746f5048d8716af546ebe1cf9",
+    "url": "/static/media/3.c6596c07.jpg"
+  },
+  {
+    "revision": "47bb22bae5a1635423e8d2a7786b953f",
+    "url": "/static/media/4.47bb22ba.jpg"
+  },
+  {
+    "revision": "714987a01919d068858c4d49aa452ee5",
+    "url": "/static/media/5.714987a0.jpg"
+  },
+  {
+    "revision": "7000584cf033f4bdec2c0657d3d7563e",
+    "url": "/static/media/6.7000584c.jpg"
+  },
+  {
+    "revision": "85b1cce26843f94926179b3dc87e97f1",
+    "url": "/static/media/7.85b1cce2.jpg"
+  },
+  {
+    "revision": "3be2a9d65ea0e5f19e3e5a5a1e9e7198",
+    "url": "/static/media/8.3be2a9d6.jpg"
+  },
+  {
+    "revision": "704dd37ec30f1d8c319c626f0675566a",
+    "url": "/static/media/Widok 1.704dd37e.jpeg"
+  },
+  {
+    "revision": "146a9a0a11bbe69afda03169427c01d8",
+    "url": "/static/media/Widok 2.146a9a0a.jpeg"
+  },
+  {
+    "revision": "a9c7a60933a04fd4e5986fab8940f466",
+    "url": "/static/media/Widok 3.a9c7a609.jpeg"
+  },
+  {
+    "revision": "e62a4bd2cabffb872d36f033dfc22ece",
+    "url": "/static/media/Widok 4.e62a4bd2.jpeg"
+  },
+  {
+    "revision": "5d2c25575f1251f99e26c9c398b99bdd",
+    "url": "/static/media/Widok 5.5d2c2557.jpeg"
+  },
+  {
+    "revision": "51b637f677ac1aafe0ffa7941970e60f",
+    "url": "/static/media/Widok 6.51b637f6.jpeg"
+  },
+  {
+    "revision": "8431e20d4b69a7ca14ccb0e4023376aa",
+    "url": "/static/media/Widok 7.8431e20d.jpeg"
+  },
+  {
+    "revision": "6f06ea56b445e7237776485a69ecd5e4",
+    "url": "/static/media/Widok 8.6f06ea56.jpeg"
+  },
+  {
+    "revision": "d9715d5e1de892448c6869fb128320a1",
+    "url": "/static/media/Widok 9.d9715d5e.jpeg"
+  },
+  {
+    "revision": "3a7bb9b830a3d9d53e7c35dd56525fc0",
+    "url": "/static/media/elew6.3a7bb9b8.jpg"
+  },
+  {
+    "revision": "802232ebe1c53de448f2a5c2313c6145",
+    "url": "/static/media/elewacja1.802232eb.jpg"
+  },
+  {
+    "revision": "1e6b31c90e6869f8d8a152d011cff6bb",
+    "url": "/static/media/elewacja2.1e6b31c9.jpg"
+  },
+  {
+    "revision": "bec08b3ae389e012d3e7f45bda79bed2",
+    "url": "/static/media/elewacja3.bec08b3a.jpg"
+  },
+  {
+    "revision": "01a434ddbc332e7408c8fcf5d66d806e",
+    "url": "/static/media/elewacja4.01a434dd.jpg"
+  },
+  {
+    "revision": "74c8f82221a31d598e5fd1ab5b486267",
+    "url": "/static/media/elewacja5.74c8f822.jpg"
+  },
+  {
+    "revision": "6682bcb930af965e3f33771d29374015",
+    "url": "/static/media/logo.6682bcb9.png"
+  },
+  {
+    "revision": "991bea1b8c8b876a15d6af7586a6d82f",
+    "url": "/static/media/photo1.991bea1b.jpeg"
+  },
+  {
+    "revision": "61e2901a34fe7f068ba75cb7914b82fc",
+    "url": "/static/media/photo3.61e2901a.jpeg"
+  },
+  {
+    "revision": "25872d503474c59389f32e475b649d37",
+    "url": "/static/media/schemat1.25872d50.png"
+  },
+  {
+    "revision": "04e6c422dab928d21037a65a705a2fe6",
+    "url": "/static/media/schemat2.04e6c422.png"
+  },
+  {
+    "revision": "f88a4924326a11de8df9c544f706b4b3",
+    "url": "/static/media/schemat3.f88a4924.jpg"
+  }
+]);
